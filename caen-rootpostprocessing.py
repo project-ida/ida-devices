74c6e2533a3eaac1993dc32b6311458cd2131e3c
Matt Lilley <m.k.lilley@gmail.com>
2025-06-25 09:50:37 +0100

    Fixes CSV pgpathth in postprocessing of root files

--- caen-rootpostprocessing.py
+++ caen-rootpostprocessing.py
@@ -52,7 +52,7 @@ from psql_credentials import PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD
 # Dynamically set CSV path with override capability
 if IS_COLAB:
     # Default CSV path
-    csv_path = '/content/drive/MyDrive/Nucleonics/Colab Notebooks/Data/processed_files.csv'
+    csv_path = '/content/drive/MyDrive/Nucleonics/Analysis/Colab Notebooks/Data/processed_files.csv'
     # Check if csv_path is overridden in the global namespace by looking for processed_files_path
     if 'processed_files_path' in globals():
         csv_path = globals()['processed_files_path']
